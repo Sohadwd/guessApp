eba55d4b21416406ace17fc771b0c3c50581371e
Sohad Dader <sohad@leannode.com>
2019-11-27 00:01:44 +0200

    add login to game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import Card from '../components/Card';
@@ -20,17 +20,35 @@ const GameScreen = props => {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
-  const nextGuessHandler = direction => {
+  const [rounds, setRounds] = useState(0);
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+  console.log('rounds', rounds);
+  /* 
+useEffect: first argument is the function it should execute after rendering the component, 
+second argument you passed to useEffect is an array of dependencies of this function
+and here you have to specify any value that's coming  from outside of this effect function.
+  */
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(rounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
+
+  // 1. generate the next guess
+  const nextGuessHandler = type => {
     if (
-      (direction === 'lower' && currentGuess < props.userChoice) ||
-      (direction === 'greater' && currentGuess > props.userChoice)
+      (type === 'lower' && currentGuess < props.userChoice) ||
+      (type === 'greater' && currentGuess > props.userChoice)
     ) {
       Alert.alert("Don't lie!", 'You know that this is wrong...', [
         { text: 'Sorry!', style: 'cancel' }
       ]);
       return;
     }
-    if (direction === 'lower') {
+    if (type === 'lower') {
       currentHigh.current = currentGuess;
     } else {
       currentLow.current = currentGuess;
@@ -41,7 +59,7 @@ const GameScreen = props => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
-    setRounds(curRounds => curRounds + 1);
+    setRounds(cur => cur + 1);
   };
 
   return (
